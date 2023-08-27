745c2516f802104ac7902913f5220a9fb6c96bac
klaus Kindermann <klauskindermann94@gmail.com>
2023-08-27 07:24:58 -0300

    Rotas Criadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PublicationsModule } from './publications/publications.module';
+import { MediasModule } from './medias/medias.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [PublicationsModule, MediasModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
