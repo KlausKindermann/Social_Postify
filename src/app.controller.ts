47baecca36ce64c7293139cdb9331e3f45196778
klaus Kindermann <klauskindermann94@gmail.com>
2023-08-28 16:17:49 -0300

    tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,6 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get("health")
-  @HttpCode(HttpStatus.OK)
   getHealth(): string {
     return this.appService.getHealth();
   }
