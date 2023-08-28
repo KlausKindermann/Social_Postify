16ce7c219ee1f8827c12a9429016f8790930a584
klaus Kindermann <klauskindermann94@gmail.com>
2023-08-28 09:31:03 -0300

    publications

--- src/publications/dto/create-publication.dto.ts
+++ src/publications/dto/create-publication.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePublicationDto {}
+import { IsNumber, IsDate, IsNotEmpty } from 'class-validator';
+
+export class CreatePublicationDto {
+  @IsNumber()
+  id?: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  mediaId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  postId: number;
+
+  @IsDate()
+  date?: Date;
+}
--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PublicationsService } from './publications.service';
 import { PublicationsController } from './publications.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PublicationsRepository } from './publication.repository';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PublicationsController],
-  providers: [PublicationsService],
+  providers: [PublicationsService, PublicationsRepository],
+  exports: [PublicationsService]
 })
 export class PublicationsModule {}
