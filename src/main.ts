dc59ad323d1861e7fbab2f1a402dfd5c8ba977b9
klaus Kindermann <klauskindermann94@gmail.com>
2023-09-10 21:58:50 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log("Server is up and running on port: " + port);
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Postify - Rest API')
+    .setDescription('Postify API description')
+    .setVersion('1.0')
+    .addTag('Postify')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
-bootstrap();
+bootstrap()
